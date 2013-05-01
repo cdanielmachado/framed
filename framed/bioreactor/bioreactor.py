962a2740237a1b60cdeb33b484447fabac0985e4
Kai Zhuang <kai.hua.zhuang@gmail.com>
2013-05-01 22:36:57 +0200

    created bioreactor.py
    bioreactor.py contains classes for bioreactor modeling

---
+++ framed/bioreactor/bioreactor.py
@@ -0,0 +1,16 @@
+__author__ = 'kaizhuang'
+
+"""
+This module defines the classes for bioreactor modeling and simulation
+"""
+
+from ..core import models
+
+class Bioreactor:
+    """
+    This class describes a generic bioreactor.
+    """
+    def __init__(self, organisms, metabolites):
+        
+        pass
+    
