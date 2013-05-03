501f952a0273de4fc2d662fb121a4eb9baa14041
Kai Zhuang <kaizhuang@Kais-MacBook-Pro.local>
2013-05-03 15:03:39 +0200

    FBA in basic.py
    doing some basic FBA in basic.py

---
+++ examples/basics.py
@@ -0,0 +1,11 @@
+from framed.io_utils.sbml import *
+from framed.core.fixes import fix_bigg_model
+from framed.analysis.fba import FBA
+
+ec_core_model = 'models/ecoli_core_model.xml'
+
+#load SBML model, and fix BIGG model's perculiarities
+model = load_sbml_model(ec_core_model, kind=CONSTRAINT_BASED)
+fix_bigg_model(model)
+
+solution = FBA(model)
