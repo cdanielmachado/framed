73ab3c1718e7538bf7a8699b8caa3c7ed7e7cb00
Kai Zhuang <kai.hua.zhuang@gmail.com>
2013-05-12 16:08:21 +0200

    added tests/fixtures directory
    moved ec_core_model.txt into the fixtures diectory

--- framed/tests/test_mdfba.py
+++ framed/tests/test_mdfba.py
@@ -1,1 +1,12 @@
 __author__ = 'kaizhuang'
+
+import unittest
+
+from framed.io_utils.plaintext import *
+from framed.core.fixes import fix_bigg_model
+
+PLAIN_TEST_MODEL = ''
+class PlainTextTest(unittest.TestCase):
+
+    def testRun(self):
+        read_model_from_file(PLAIN_TEXT_MODEL, kind=CONSTRAINT_BASED)
