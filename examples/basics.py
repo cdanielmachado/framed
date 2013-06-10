2011f97eaaab35a111195d25e18db06b7560aebd
Kai Zhuang <kaizhuang@Kais-MacBook-Pro.local>
2013-06-10 14:28:47 +0200

    fixed import in examples

--- examples/basics.py
+++ examples/basics.py
@@ -1,6 +1,6 @@
 from framed.io_utils.sbml import *
 from framed.core.fixes import fix_bigg_model
-from framed.analysis.fba import FBA
+from framed.analysis.simulation import FBA
 
 ec_core_model = 'models/ecoli_core_model.xml'
 
@@ -9,3 +9,4 @@ model = load_sbml_model('models/ecoli_core_model.xml', kind=CONSTRAINT_BASED)
 fix_bigg_model(model)
 
 solution = FBA(model)
+
