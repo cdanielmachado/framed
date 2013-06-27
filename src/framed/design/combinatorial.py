d96681db5e291279f2310a3144db651dad2e235c
Daniel Machado <daniel@Daniels-MacBook-Air.local>
2013-06-27 14:15:53 +0200

    Some changes in combinatorial deletion method.

--- src/framed/design/combinatorial.py
+++ src/framed/design/combinatorial.py
@@ -20,6 +20,7 @@
 '''
 
 from itertools import combinations
+from collections import OrderedDict
 from ..core.models import GPRConstrainedModel
 from ..analysis.deletion import deletion
 from ..analysis.simulation import FBA
@@ -29,7 +30,7 @@ from ..analysis.essentiality import essentiality
 
 
 
-def combinatorial_gene_deletion(model, objective, max_dels, method='FBA', targets=None, min_growth=0.01, abstol=1e-4):
+def combinatorial_gene_deletion(model, objective, max_dels, targets=None, method='FBA', reference=None, min_growth=0.1, abstol=1e-3):
     """ Compute solutions for a set of combinatorial gene deletions.
     
     Arguments:
@@ -45,10 +46,10 @@ def combinatorial_gene_deletion(model, objective, max_dels, method='FBA', target
         list (of (list of str, float)) -- valid solutions
     """    
     
-    return combinatorial_deletion(model, objective, max_dels, 'genes', method, targets, min_growth, abstol)
+    return combinatorial_deletion(model, objective, max_dels, 'genes', targets, method, reference, min_growth, abstol)
 
 
-def combinatorial_reaction_deletion(model, objective, max_dels, method='FBA', targets=None, min_growth=0.01, abstol=1e-4):
+def combinatorial_reaction_deletion(model, objective, max_dels, targets=None, method='FBA', reference=None, min_growth=0.1, abstol=1e-3):
     """ Compute solutions for a set of combinatorial reaction deletions.
     
     Arguments:
@@ -64,10 +65,10 @@ def combinatorial_reaction_deletion(model, objective, max_dels, method='FBA', ta
         list (of (list of str, float)) -- valid solutions
     """    
     
-    return combinatorial_deletion(model, objective, max_dels, 'reactions', method, targets, min_growth, abstol)
+    return combinatorial_deletion(model, objective, max_dels, 'reactions', targets, method, reference, min_growth, abstol)
 
 
-def combinatorial_deletion(model, objective, max_dels, kind='reactions', method='FBA', targets=None, min_growth=0.01, abstol=1e-4):
+def combinatorial_deletion(model, fobj, max_dels, kind='reactions', targets=None, method='FBA', reference=None, min_growth=0.1, abstol=1e-3):
     """ Generic interface for computing for a set of combinatorial gene or reaction deletions.
     
     Arguments:
@@ -93,30 +94,41 @@ def combinatorial_deletion(model, objective, max_dels, kind='reactions', method=
     solver = solver_instance()
     solver.build_problem(model)
     
-    wt_solution = FBA(model, solver=solver)
-    wt_fluxes = wt_solution.values
-    wt_growth = wt_solution.fobj
+    if not reference:
+        wt_solution = FBA(model, solver=solver)
+        reference = wt_solution.values
+    
     biomass = model.detect_biomass_reaction()
+    wt_growth = reference[biomass]
+    wt_fval = fobj(reference)
 
     # for single deletions there is no gain in computing all essential genes/reactions
     if max_dels > 1:
         essential = essentiality(model, kind, min_growth)
         targets = [target for target in targets if target not in essential]
             
-    del_sets = [x for x in combinations(targets, max_dels)]
-#    del_sets = [del_set for i in range(max_dels) for del_set in combinations(targets, i + 1)]
+#    del_sets = [x for x in combinations(targets, max_dels)]
+    del_sets = [del_set for i in range(max_dels) for del_set in combinations(targets, i + 1)]
             
-    
-    solutions = []
-    
-    fobj = lambda v: sum([coeff*v[r_id] for r_id, coeff in objective.items()])
+    solutions = dict()
 
     for del_set in del_sets:
-        solution = deletion(model, del_set, kind, method, wt_fluxes, solver)
+        solution = deletion(model, del_set, kind, method, reference, solver)
     
         if solution and solution.status == Status.OPTIMAL:
             fval = fobj(solution.values)
-            if fval > abstol and solution.values[biomass] >= min_growth * wt_growth:
-                solutions.append((del_set, fval))
-            
+            if fval > wt_fval + abstol and solution.values[biomass] >= min_growth * wt_growth and not _redundant(del_set, fval, solutions, abstol):
+                solutions[del_set] = fval
+    
+    solutions = OrderedDict(sorted(solutions.items(), key=lambda (_, fval): fval, reverse=True))
     return solutions
+
+
+def _redundant(del_set, fval, solutions, abstol):
+    redundant = False
+    del_set = set(del_set)
+    for previous, fval0 in solutions.items():
+        if del_set.issuperset(set(previous)) and fval < fval0 + abstol:
+            redundant = True
+            break
+    return redundant
