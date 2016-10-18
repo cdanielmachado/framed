c64b673fd576e26bbf3f29bd855e50b6ad882177
Daniel <cdanielmachado@gmail.com>
2016-10-18 17:55:41 +0200

    fix problem setting default solver

--- src/framed/solvers/__init__.py
+++ src/framed/solvers/__init__.py
@@ -1,27 +1,9 @@
-""" 
+"""
 Package implementing interfaces to common solvers.
 
 @author: Daniel Machado
 
-   Copyright 2013 Novo Nordisk Foundation Center for Biosustainability,
-   Technical University of Denmark.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
 """
-
-from warnings import warn
-from .solver import Parameter
 
 solvers = dict()
 
@@ -55,14 +37,32 @@ try:
 except:
     pass
 
-default_solver = 'gurobi'
+
+default_solver = None
+
+
+def get_default_solver():
+
+    global default_solver
+
+    solver_order = ['gurobi', 'cplex', 'glpk', 'glpk_lazy']
+
+    for solver in solver_order:
+        if solver in solvers.keys():
+            default_solver = solver
+            break
+
+    if not default_solver:
+        print 'Error: No solver available.'
+
+    return default_solver
 
 
 def set_default_solver(solvername):
     """ Sets default solver.
 
     Arguments:
-        solvername : str -- solver name (currently available: gurobi, pulp)
+        solvername : str -- solver name (currently available: gurobi, cplex, glpk)
     """
 
     global default_solver
@@ -70,7 +70,7 @@ def set_default_solver(solvername):
     if solvername.lower() in solvers.keys():
         default_solver = solvername.lower()
     else:
-        warn('Solver ' + solvername + ' not available.')
+        print 'Error: solver ' + solvername + ' not available.'
 
 
 def solver_instance(model=None):
@@ -83,4 +83,7 @@ def solver_instance(model=None):
         Solver
     """
 
-    return solvers[default_solver](model)
+    solver = get_default_solver()
+
+    if solver:
+        return solvers[solver](model)
