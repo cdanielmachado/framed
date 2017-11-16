5dd04d2167a71f57a904c8bcd238d68abd6a2acf
Daniel <cdanielmachado@gmail.com>
2017-11-16 13:43:28 +0100

    version bump to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'framed',
-    version = '0.3.5',
+    version = '0.4.0',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = ["numpy",
